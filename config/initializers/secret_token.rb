9d50730d8bcba72941383e9c8aa5d7e7feee6729
William Gasner <willgasner@gmail.com>
2014-02-07 17:51:58 -0800

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WillsApp::Application.config.secret_key_base = 'c91d2e73c2580e7e2b5e3cbccff3bce2cff8b4dea4bf46c53f2075e8e33a45ee5355d7bcf369cb8f26b5cb5e7277824be099df53043d03e49cc8b389fdb994d6'
